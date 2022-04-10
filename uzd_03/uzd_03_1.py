1709bb859d396a061f511978ba38820eafd698fd
Oksana <oksana.tkaca@gmail.com>
2022-04-10 20:23:47 +0300

    Uzd_03_1 un 2 Risināšana

--- uzd_03/uzd_03_1.py
+++ uzd_03/uzd_03_1.py
@@ -1,2 +1,35 @@
-# Apgriezt pozitīva skaitļa ciparu secību. Piemērs: 12345 => 54321
+# Apgriezt pozitīva skaitļa ciparu secību.
+# Piemērs: 12345 => 54321
 
+
+#  ????!!!!!!  end= ""   nākomo k.rindu pievelk klāt !!!!!! ??????
+x=(input("Ievadiet pozitīvu skaitli: "))
+
+
+#a=int(x)
+#for a in range(len(x)-1, -1, -1):
+#    print(x[a], end="")
+
+
+
+#x=int(input("Ievadiet pozitīvu skaitli: "))
+#for y in range(1, x+1):
+#    print(y, end="")
+
+
+
+
+
+
+
+
+
+x=input("Ievadi pozitīvu skaitli: ")
+try:
+    y=int(x)
+    if y>0:
+        for i in range(len(x)-1, -1, -1):
+            print(x[i], end="") # end lai liek rindā ciparus vienu aiz otra
+    else: print("Nav iev. poz. sk!!!")
+except ValueError:
+    print("Nav ievadīts skaitlis.")
--- uzd_03/uzd_03_2.py
+++ uzd_03/uzd_03_2.py
@@ -1,2 +1,31 @@
 # Atrast cik skaitļi norādītajā intervālā dalās ar lietotāja norādīto dalītāju.
 
+# OK
+x=int(input("Ievadiet pirmo intervāla skaitli: "))
+y=int(input("Ievadiet pēdējo intervāla skaitli: "))
+z=int(input("Ievadiet dalītāju: "))
+
+
+
+cikskaitli=0                #Obligāti jānorāda sākuma punkts, lai zin no cik +=1
+for a in range(x, y+1):
+    if a%z==0:
+        cikskaitli+=1
+print(f"Robežās starp {x} un {y} ar {cikskaitli} skaitļiem dalās ar dalītāju {z}")
+
+
+
+
+
+
+
+
+x=int(input("Sākuma skaitlis: "))
+y=int(input("Beigu skaitlis: "))
+z=int(input("Dalītājs: "))
+
+cik=0           #jānorāda sākuma skaitlis
+for i in range(x, y+1):
+    if i%z==0:
+        cik+=1  # cik=cik+1
+print(f"Robežās starp {x} un {y} - {cik} skaitļi dalās ar {z}")
